86e0402288043dabbabe43edd04d6703be59121b
Ana Rosa <anarosalinsemello@gmail.com>
2020-05-16 03:04:25 -0300

    Adicionando Data e Hora

--- CalculadoraJS/Scripts/controller/calcController.js
+++ CalculadoraJS/Scripts/controller/calcController.js
@@ -1,7 +1,7 @@
 class CalcController {
 
     constructor(){
-
+        this._locale = "pt-BR";
         this._displayCalcEl = document.querySelector("#display"); 
         this._dateEl = document.querySelector("#data"); 
         this._timeEl = document.querySelector("#hora"); 
@@ -11,20 +11,49 @@ class CalcController {
 
     initialize(){
 
-        this._dateEl.innerHTML = "4567";
-        this._timeEl.innerHTML ="4567";
+        this.setDisplayDateTime();
+
+        //Arrow Function: ()=>{}
+        setInterval(()=>{
+
+            this.setDisplayDateTime();
+
+        }, 1000);
+    }
+
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+    }
+
+    get displayTime(){  
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        this._dateEl.innerHTML = value;
     }
 
     get displayCalc(){
         return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor){
-        this._displayCalcEl.innerHTML = valor;
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
 
     get currentDate(){
-        return this._currentDate;
+        return new Date();
     }
 
     set currentDate(valor){
