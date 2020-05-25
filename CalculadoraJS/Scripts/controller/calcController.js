ef320959398676bd82f1f73863590a4677ac334f
Ana Rosa <anarosalinsemello@gmail.com>
2020-05-24 23:35:32 -0300

    definindo limite

--- CalculadoraJS/Scripts/controller/calcController.js
+++ CalculadoraJS/Scripts/controller/calcController.js
@@ -496,6 +496,12 @@ class CalcController {
     }
 
     set displayCalc(value){
+
+        if (value.toString().length > 10){
+            this.setError();
+            return
+        }
+
         this._displayCalcEl.innerHTML = value;
     }
 
